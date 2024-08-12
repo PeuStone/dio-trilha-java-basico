2031d62ed4400121c3b4f36f5980cebe9bcd2273
PeuStone <pedrohmotaaa@gmail.com>
2024-08-12 11:09:33 -0300

    Primeiros testes em relação a tipos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
     }
 }
