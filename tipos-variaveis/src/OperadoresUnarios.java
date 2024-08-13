65a5b06a45ed88389cf00a4b9d3289e5ab63506d
PeuStone <pedrohmotaaa@gmail.com>
2024-08-13 10:19:12 -0300

    Demonstrações de operadores unários e diferentes aplicações

---
+++ tipos-variaveis/src/OperadoresUnarios.java
@@ -0,0 +1,34 @@
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+        // negação / inversão de sinais
+
+        // int numero = 5;
+        //
+        // numero = - numero;
+        //
+        // System.out.println(- numero);
+        //
+        // System.out.println(numero);
+        //
+        // numero = numero * -1;
+        //
+        // System.out.println(numero);
+
+
+        //x incrementação / decrementação
+        //int numero = 5;
+
+        // numero++;
+        // numero = numero +1; é a mesma coisa da linha de cima.
+
+        // System.out.println(numero);
+
+        // boolean
+
+        boolean variavel = true;
+
+        variavel = !variavel; // forma de inverter valores booleanos
+
+        System.out.println(variavel);
+    }
+}
